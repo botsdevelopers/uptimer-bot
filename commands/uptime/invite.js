58628425054da325b44cb12dde6862e0be2a6c64
TEDDY <75193966+TEDDY6969@users.noreply.github.com>
2022-01-15 14:34:55 +0530

    Create invite.js

---
+++ commands/uptime/invite.js
@@ -0,0 +1,33 @@
+const { Client, MessageActionRow, MessageButton, CommandInteraction } = require("discord.js");
+
+const { botid } = require("../../config.json");
+
+module.exports = {
+
+  name: "invite",
+
+  description: "invite me",
+
+      ownerOnly: false,  
+
+  run: async (client, message, args) => {
+
+          
+
+const row = new MessageActionRow().addComponents(
+
+      new MessageButton()        
+
+        .setURL(`https://discord.com/api/oauth2/authorize?client_id=${botid}&permissions=8&scope=bot%20applications.commands`)
+
+        .setLabel("INVITE")
+
+            .setStyle("LINK")
+
+    )
+
+          message.channel.send({ content: `Thanks for using mee 🌸\nTap the below link to invite me`, components: [row] })
+
+  }
+
+}
