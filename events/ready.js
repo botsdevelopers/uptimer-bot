e2529d233afb50c9130a42657661a01b48d08d9a
TEDDY <75193966+TEDDY6969@users.noreply.github.com>
2022-01-15 14:00:17 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,20 @@
 const client = require("../index.js");
+const UrlsConfig = require('../database/models/UrlsConfig');
 client.on("ready", () => 
-      console.log(`Successfully Logged in as ${client.user.tag}`)
-         );
+      console.log(`Successfully Logged in as ${client.user.tag}`),
+         
+setInterval(async () => {
+	UrlsConfig.countDocuments({}, (err, total) => {
+		client.projectsSize = total;
+		client.user.setActivity(
+			`${total} Project(s) on ${client.guilds.cache.size} servers`,
+			{
+				type: 'WATCHING'
+			}
+		);
+	});
+})
+          );
+  
 
-/* Made By REAL GAMING */
+
