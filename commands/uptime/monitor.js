affd4232e076f319d8c40f652314dd0d2abf5539
TEDDY <75193966+TEDDY6969@users.noreply.github.com>
2022-01-15 14:32:21 +0530

    Update monitor.js

--- commands/uptime/monitor.js
+++ commands/uptime/monitor.js
@@ -4,18 +4,20 @@ const fetch = require("node-fetch");
 const validUrl = require("valid-url");
 
 module.exports = {
-  name: "monitor",
+  name: "add",
   description: "Adds monitor to your project.",    
   ownerOnly: false,
   run: async (client, message, args) => {
+    
     var url = args[0];
 
-    if (!url) return message.reply({ content: "Please provide a project url" });
+const botInServer = message.guild.members.cache.get(client.user.id);
+    if (!url) return message.reply({ embeds: [new MessageEmbed().setTitle("Error | Provide a URL").setDescription("Please provide the url of your project you want to host here. ").setColor("RED").setFooter(message.member.user.tag).setThumbnail(client.user.displayAvatarURL()).setTimestamp()] });
     if (!validUrl.isUri(url)) {
-      return message.channel.send({ content: "Please provide a vaild url" });
+      return message.channel.send({ embeds: [new MessageEmbed().setTitle("Error | Provide a valid URL").setDescription("Please provide the a valid url of your project you want to host here. ").setColor("RED").setFooter(message.member.user.tag).setThumbnail(client.user.displayAvatarURL()).setTimestamp()] });
     }
         
-    var messageAlert = await message.channel.send({ content: `<@${message.member.user.id}>`, embeds: [new MessageEmbed().setColor("WHITE").setDescription("<a:loading:879577672303378442> Please wait...").setFooter(message.member.user.tag).setThumbnail(message.member.user.displayAvatarURL())] });
+    var messageAlert = await message.channel.send({ content: `<@${message.member.user.id}>`, embeds: [new MessageEmbed().setColor("WHITE").setDescription("<a:time:746937170685722634> Please wait...").setFooter(message.member.user.tag).setThumbnail(message.member.user.displayAvatarURL())] });
 
     var checkIfExsists = await UrlsConfig.findOne({
       projectURL: url,
@@ -43,13 +45,21 @@ module.exports = {
           message.reply("Fetching Error");
         }
         
-        await messageAlert.edit({ embeds: [new MessageEmbed().setTitle("<a:tick:879317182721454160> Added Succesfully").setColor("RED").setDescription("Thanks for using me").setTimestamp()] });
-        return message.delete();
-      });
+        await messageAlert.edit({ embeds: [new MessageEmbed().setTitle("<a:r2:886915489190805515> Added Succesfully").setColor("GREEN").setDescription("Thanks for using me").setTimestamp()] });
+        if (botInServer.permissions.has('ADMINISTRATOR') || !botInServer.permissions.has('MANAGE_MESSAGE')) {
+            return message.delete();
+        
+             
     } else {      
       
       await messageAlert.edit({ embeds: [new MessageEmbed().setTitle("Error | Already Registered").setDescription("The Project you're Trying To Register Is Already In The Database").setColor("RED").setFooter(message.member.user.tag).setThumbnail(client.user.displayAvatarURL()).setTimestamp()] });
-      return message.delete();
+          if (botInServer.permissions.has('ADMINISTRATOR') || !botInServer.permissions.has('MANAGE_MESSAGE')) {
+            return message.delete();
     }
-  },
-};
+        }
+      });
+    };
+  }
+}
+          
+  
